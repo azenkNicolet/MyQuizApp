dc46474f38718def999de3d0540625dc32ec673a
Al Zenk <azenk@nicoletcollege.edu>
2023-12-14 15:39:06 -0600

    put extras now referenced in ResultActivity. Clicking finish button returns user to main activity.

--- app/src/main/java/com/zenk/myquizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/zenk/myquizapp/QuizQuestionsActivity.kt
@@ -1,5 +1,6 @@
 package com.zenk.myquizapp
 
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
@@ -17,6 +18,8 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
     private var mCurrentPosition: Int = 1
     private var mQuestionsList: ArrayList<Question>? = null
     private var mSelectedOptionPosition: Int = 0
+    private var mUserName : String? = null
+    private var mCorrectAnswers: Int = 0
 
     private var progressBar: ProgressBar? = null
     private var tvProgress: TextView? = null
@@ -32,6 +35,8 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
+
+        mUserName = intent.getStringExtra(Constants.USER_NAME)
 
         progressBar = findViewById(R.id.progressBar)
         tvProgress  = findViewById(R.id.tv_progress)
@@ -162,7 +167,12 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
                         }
 
                         else -> {
-                            Toast.makeText(this, "You made it to the end", Toast.LENGTH_SHORT)
+                            val intent = Intent(this, ResultActivity::class.java)
+                            intent.putExtra(Constants.USER_NAME, mUserName)
+                            intent.putExtra(Constants.CORRECT_ANSWERS, mCorrectAnswers)
+                            intent.putExtra(Constants.TOTAL_QUESTIONS, mQuestionsList?.size)
+                            startActivity(intent)
+                            finish()
                         }
                     }
 
@@ -175,6 +185,11 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
                     {
                         //If wrong answer is selected, highlight it by setting background to red.
                         answerView(mSelectedOptionPosition, R.drawable.wrong_option_border_bg)
+                    }
+                    else
+                    {
+                        //answer was correct. Increment variable tracking number of correct answers.
+                        mCorrectAnswers++
                     }
 
                     //Correct option always gets green background.
--- app/src/main/java/com/zenk/myquizapp/ResultActivity.kt
+++ app/src/main/java/com/zenk/myquizapp/ResultActivity.kt
@@ -1,11 +1,35 @@
 package com.zenk.myquizapp
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class ResultActivity : AppCompatActivity() {
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        val tvName: TextView = findViewById(R.id.tv_name)
+        val tvScore: TextView = findViewById(R.id.tv_score)
+        val btnFinish: Button = findViewById(R.id.btn_finish)
+
+        tvName.text = intent.getStringExtra(Constants.USER_NAME).toString()
+        /*tvScore.text = "Your Score is " +
+                intent.getIntExtra(Constants.CORRECT_ANSWERS, 0).toString() + " out of " +
+                intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0).toString() + "!" */
+        val correctAnswers: String = intent.getIntExtra(Constants.CORRECT_ANSWERS,0).toString()
+        val totalQuestions: String = intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0).toString()
+
+        tvScore.text = "Your Score is $correctAnswers out of $totalQuestions"
+
+        btnFinish.setOnClickListener{
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
     }
 }
